711af4b3cadddb513d053415910c423937e8b0a4
Mati Ps <matiaslisandro9@gmail.com>
2024-09-05 08:34:05 -0300

    CRUD-created

---
+++ CRUD/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.dgvPersonas = new System.Windows.Forms.DataGridView();
+            this.Precio = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(173, 24);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(417, 38);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(173, 68);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(417, 38);
+            this.txtMarca.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Marca";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(39, 698);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(147, 61);
+            this.btnInsertar.TabIndex = 4;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(192, 698);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(147, 61);
+            this.btnModificar.TabIndex = 5;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(39, 778);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(147, 61);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(345, 698);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(147, 61);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(192, 778);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(147, 61);
+            this.btnNuevo.TabIndex = 7;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // dgvPersonas
+            // 
+            this.dgvPersonas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersonas.Location = new System.Drawing.Point(39, 195);
+            this.dgvPersonas.Name = "dgvPersonas";
+            this.dgvPersonas.RowHeadersWidth = 102;
+            this.dgvPersonas.RowTemplate.Height = 40;
+            this.dgvPersonas.Size = new System.Drawing.Size(1077, 406);
+            this.dgvPersonas.TabIndex = 8;
+            // 
+            // Precio
+            // 
+            this.Precio.AutoSize = true;
+            this.Precio.Location = new System.Drawing.Point(46, 112);
+            this.Precio.Name = "Precio";
+            this.Precio.Size = new System.Drawing.Size(95, 32);
+            this.Precio.TabIndex = 9;
+            this.Precio.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(173, 112);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(417, 38);
+            this.txtPrecio.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 866);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.Precio);
+            this.Controls.Add(this.dgvPersonas);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.DataGridView dgvPersonas;
+        private System.Windows.Forms.Label Precio;
+        private System.Windows.Forms.TextBox txtPrecio;
+    }
+}
+
---
+++ CRUD/Form1.cs
@@ -0,0 +1,94 @@
+﻿using CRUD.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CargarDatos()
+        {
+            using (NegocioEntities1 db = new NegocioEntities1())
+            {
+                dgvPersonas.DataSource = db.Productos.ToList();
+            }
+        }
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            using (NegocioEntities1 db = new NegocioEntities1())
+            {
+                Producto producto = new Producto();
+                producto.Nombre = txtNombre.Text;
+                producto.Marca = txtMarca.Text;
+                producto.Precio = txtPrecio.Text.Length;
+                db.Productos.Add(producto);
+                db.SaveChanges();
+                MessageBox.Show("Se agergo con exito el producto");
+            }
+            this.CargarDatos();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dgvPersonas.CurrentRow.Cells["id"].Value);
+            int precio = Convert.ToInt32(txtPrecio.Text);
+            using (NegocioEntities1 db = new NegocioEntities1())
+            {
+                Producto producto = new Producto();
+                producto.Id = id;
+                producto.Nombre = txtNombre.Text;
+                producto.Marca = txtMarca.Text;
+                producto.Precio = precio;
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            this.CargarDatos();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dgvPersonas.CurrentRow.Cells["id"].Value);
+            using (NegocioEntities1 db = new NegocioEntities1())
+            {
+                var lst = db.Productos.Where(x => x.Id == id).FirstOrDefault();
+                db.Productos.Remove(lst);
+                db.SaveChanges();
+            }
+            this.CargarDatos();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            using (NegocioEntities1 db = new NegocioEntities1())
+            {
+                var buscar = db.Productos.Where(x => x.Nombre == txtNombre.Text || x.Marca == txtMarca.Text);
+                dgvPersonas.DataSource = buscar.ToList();
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtMarca.Text = "";
+            txtNombre.Text = "";
+            txtPrecio.Text = "";
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            CargarDatos();
+        }
+    }
+}
